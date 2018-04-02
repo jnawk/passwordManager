d508fb16bfeda2ae56a7fe5f1d08625f68fd4742
Philip Dowie <philip@jnawk.nz>
2018-04-02 13:21:39 +1200

    added skeleton JSX

---
+++ js/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+import PasswordManager from './PasswordManager.jsx';
+
+const passwordManager = (
+    <PasswordManager
+        v1Endpoint='https://pm.jnawk.nz/passwordManager/p/json'
+        v2Endpoint='https://h2gsweotlb.execute-api.ap-southeast-2.amazonaws.com/p'/>
+);
+render(passwordManager, document.getElementById('PasswordManager'));

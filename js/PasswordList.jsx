fad7ec25dee173ee620d8d91db21cad594926a0c
Philip Dowie <philip@jnawk.nz>
2018-04-02 22:06:32 +1200

    added skeleton code for password list

---
+++ js/PasswordList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PasswordList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>password list</div>;
+    }
+}
+
+export default PasswordList;

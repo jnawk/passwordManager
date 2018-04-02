0ba8694b772e11a809a3fb46b4fa0d4e6261b567
Philip Dowie <philip@jnawk.nz>
2018-04-02 22:05:50 +1200

    updated v1 endpoint to through new API gateway

--- js/index.jsx
+++ js/index.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './passwordManager.css';
 const passwordManager = (
     <PasswordManager
-        v1Endpoint='https://pm.jnawk.nz/passwordManager/p/json'
+        v1Endpoint='https://75e27ktocj.execute-api.ap-southeast-2.amazonaws.com/p'
         v2Endpoint='https://h2gsweotlb.execute-api.ap-southeast-2.amazonaws.com/p'/>
 );
 render(passwordManager, document.getElementById('PasswordManager'));

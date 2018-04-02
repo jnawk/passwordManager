c9552aae10da2f5cfcc71abe538c1638a4bee774
Philip Dowie <philip@jnawk.nz>
2018-04-02 22:03:32 +1200

    fixed grid layout and updated heading for v2

--- js/PasswordManager.jsx
+++ js/PasswordManager.jsx
@@ -29,35 +29,29 @@ class PasswordManager extends React.Component {
             return <div>yay</div>;
         } else {
             const v1Login = <Login
-                title='V1 Login'
+                title='V1 Login Details'
                 callback={(param, value) => {
                     this.receive_credentials('v1', param, value);
                 }}/>;
             const v2Login = <Login
-                title='V2 Login'
+                title='V2 Login Details (if different from V1)'
                 callback={(param, value) => {
                     this.receive_credentials('v2', param, value);
                 }}/>;
             return <Grid className="show-grid">
                 <Row>
-                    <Col lg={4}>
-                        {v1Login}
-                    </Col>
+                    <Col lg={6}>{v1Login}</Col>
+                    <Col lg={6}>{v2Login}</Col>
                 </Row>
                 <Row>
-                    <Col lg={4}>
-                        {v2Login}
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={4}>
-                        <Grid>
-                            <Row>
-                                <Col lg={2} lgOffset={1}>
-                                    <Button onClick={this.loginButton_click}>Login</Button>
-                                </Col>
-                            </Row>
-                        </Grid>
+                    <Col lg={12}>
+                        <Row>
+                            <Col lg={12}>
+                                <Button onClick={this.loginButton_click}>
+                                    Login
+                                </Button>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Grid>;

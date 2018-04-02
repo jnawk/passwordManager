e6880cfd406f0c61e49286f09e675b43b63912ff
Philip Dowie <philip@jnawk.nz>
2018-04-02 22:02:15 +1200

    changed H1 to H2

--- js/Login.jsx
+++ js/Login.jsx
@@ -10,7 +10,7 @@ class Login extends React.Component {
         return <div>
             <Row>
                 <Col lg={12}>
-                    <h1>{this.props.title}</h1>
+                    <h2>{this.props.title}</h2>
                 </Col>
             </Row>
             <Row>

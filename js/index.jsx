6b4e9d723d136227bd70c79f9d5b93f73c6bb9ca
Philip Dowie <philip@jnawk.nz>
2018-04-05 23:24:23 +1200

    single endpoint now

--- js/index.jsx
+++ js/index.jsx
@@ -3,9 +3,11 @@ import {render} from 'react-dom';
 import PasswordManager from './PasswordManager.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './passwordManager.css';
+
+const endpoint = 'https://h2gsweotlb.execute-api.ap-southeast-2.amazonaws.com/p';
+
 const passwordManager = (
-    <PasswordManager
-        v1Endpoint='https://75e27ktocj.execute-api.ap-southeast-2.amazonaws.com/p'
-        v2Endpoint='https://h2gsweotlb.execute-api.ap-southeast-2.amazonaws.com/p'/>
+    <PasswordManager endpoint={endpoint}/>
 );
+
 render(passwordManager, document.getElementById('PasswordManager'));

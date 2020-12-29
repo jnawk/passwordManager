abfa69b807f9acd2e80d5bd51fcef8fd2fdedf97
Philip Dowie <philip@jnawk.nz>
2020-12-29 16:36:12 +1300

    remove old CSS

--- js/index.jsx
+++ js/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {render} from 'react-dom'
 import PasswordManager from './PasswordManager.jsx'
 import 'bootstrap/dist/css/bootstrap.css'
-import './passwordManager.css'
 
 const endpoint = 'https://h2gsweotlb.execute-api.ap-southeast-2.amazonaws.com/p'
 
